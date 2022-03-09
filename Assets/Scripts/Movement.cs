c8e8a6adab732c25edefb8675547efe20d1e3c28
JohnGnrk <76203111+JohnGnrk@users.noreply.github.com>
2022-03-09 21:23:33 +0200

    Movement in Unity
    The first workshop , in which we talked about the editor, scene view basic navigation in unity and movement with C#.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    float movementSpeed = 4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 5)
+        {
+            transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -5)
+        {
+            transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && transform.position.z <5)
+        {
+            transform.position += Vector3.forward * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && transform.position.x > -5)
+        {
+            transform.position += Vector3.back * movementSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/MovementSystem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class MovementSystem : MonoBehaviour
+{
+    private Rigidbody player;
+    public float movementSpeed =50f;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        player = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.AddForce(movementSpeed * Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.AddForce(movementSpeed * Vector3.back);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.AddForce(movementSpeed * Vector3.left);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.AddForce(movementSpeed * Vector3.right);
+        }
+    }
+
+}
